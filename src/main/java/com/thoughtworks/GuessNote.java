7c7316ae465943dfb7f9cbb81242c21bf469dbb9
Ryan.Wang <ranwang1995@outlook.com>
2020-02-28 21:21:04 +0800

    [WangRan] feat: add methods to check input format

--- src/main/java/com/thoughtworks/GuessNote.java
+++ src/main/java/com/thoughtworks/GuessNote.java
@@ -1,7 +1,6 @@
 package com.thoughtworks;
 
-import jdk.internal.util.xml.impl.Input;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class GuessNote {
@@ -10,6 +9,8 @@ public class GuessNote {
     private List<String> outputHistory;
 
     public GuessNote() {
+        inputHistory = new ArrayList<>();
+        outputHistory =  new ArrayList<>();
     }
 
     public int getInputCount() {
@@ -22,6 +23,48 @@ public class GuessNote {
 
     public List<String> getOutputHistory() {
         return outputHistory;
+    }
+
+    public String inputCheck(String input) {
+        if(lengthCheck(input)&&rangeCheck(input)&&rangeCheck(input)){
+            return input;
+        }
+        else {
+            return null;
+        }
+    }
+
+    private boolean lengthCheck(String input) {
+        return 4 == input.length();
+    }
+
+    private boolean rangeCheck(String input) {
+        for (int index = 0; index < input.length(); index++) {
+            if (input.charAt(index) < '1') {
+                return false;
+            }
+            if (input.charAt(index) > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean repeatCheck(String input) {
+        List<Character> charList = new ArrayList<>();
+        for (int index = 0; index < input.length(); index++) {
+            char currentChar = input.charAt(index);
+            if (charList.contains(currentChar)) {
+                return false;
+            }
+            charList.add(currentChar);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        GuessNote guessNote = new GuessNote();
+        System.out.println(guessNote.rangeCheck("1253"));
     }
 
 }
