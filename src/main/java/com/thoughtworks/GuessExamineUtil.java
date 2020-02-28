7e56921b46ceca810784820622c93ad961ddb818
Ryan.Wang <ranwang1995@outlook.com>
2020-02-28 21:41:53 +0800

    [WangRan] feat: add methods to validate input

---
+++ src/main/java/com/thoughtworks/GuessExamineUtil.java
@@ -0,0 +1,45 @@
+package com.thoughtworks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuessExamineUtil {
+
+    public static final int GUESS_COUNT = 4;
+    public static final int GUESS_LOWER_LIMIT = 1;
+    public static final int GUESS_UPPER_LIMIT = 9;
+
+
+    public static boolean inputCheck(String input) {
+        return lengthCheck(input) && rangeCheck(input) && repeatCheck(input);
+    }
+
+    private static boolean lengthCheck(String input) {
+        return GUESS_COUNT == input.length();
+    }
+
+    private static boolean rangeCheck(String input) {
+        for (int index = 0; index < input.length(); index++) {
+            if (input.charAt(index) - 48 < GUESS_LOWER_LIMIT) {
+                return false;
+            }
+            if (input.charAt(index) - 48 > GUESS_UPPER_LIMIT) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean repeatCheck(String input) {
+        List<Character> charList = new ArrayList<>();
+        for (int index = 0; index < input.length(); index++) {
+            char currentChar = input.charAt(index);
+            if (charList.contains(currentChar)) {
+                return false;
+            }
+            charList.add(currentChar);
+        }
+        return true;
+    }
+
+}
