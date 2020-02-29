d9d5af618ab9ca950bb7257dce69ca9815d7c901
Ryan.Wang <ranwang1995@outlook.com>
2020-02-29 17:55:41 +0800

    [WangRan] feat: add a constant to store the maximum times of guess allowed

--- src/main/java/com/thoughtworks/GuessConstant.java
+++ src/main/java/com/thoughtworks/GuessConstant.java
@@ -4,4 +4,5 @@ public class GuessConstant {
     public static final int COUNT = 4;
     public static final int LOWER_LIMIT = 1;
     public static final int UPPER_LIMIT = 9;
+    public static final int MAX_TIMES_OF_GUESS= 6;
 }
