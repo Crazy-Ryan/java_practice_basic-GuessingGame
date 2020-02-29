7378e860931e1cffb9e4d9db19bcae671960ccf0
Ryan.Wang <ranwang1995@outlook.com>
2020-02-29 17:01:11 +0800

    [WangRan] feat: add class AnswerGenerator

---
+++ src/main/java/com/thoughtworks/AnswerGenerator.java
@@ -0,0 +1,20 @@
+package com.thoughtworks;
+
+public class AnswerGenerator {
+    private String answer;
+
+    public AnswerGenerator() {
+    }
+
+    public AnswerGenerator(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
