56551139219e8537734cd988b0d152a954707b81
Ryan.Wang <ranwang1995@outlook.com>
2020-02-29 17:54:36 +0800

    [WangRan] feat: add a static method to read file on a specified path

---
+++ src/main/java/com/thoughtworks/ReadFileUtil.java
@@ -0,0 +1,22 @@
+package com.thoughtworks;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class ReadFileUtil {
+    public static String readFile(String path) {
+        File file = new File(path);
+        StringBuilder result = new StringBuilder();
+        try (InputStream input = new FileInputStream(file)
+        ) {
+            int byteRead;
+            while ((byteRead = input.read()) != -1) {
+                result.append((char) byteRead);
+            }
+            return result.toString();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
