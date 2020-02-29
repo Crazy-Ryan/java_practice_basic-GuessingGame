5adfb560e1b95fe673eedf5539d223920aa4821d
Ryan.Wang <ranwang1995@outlook.com>
2020-02-29 20:41:46 +0800

    [WangRan] refactor: WrongFormat renamed WrongFormatException

--- src/main/java/com/thoughtworks/WrongFormat.java
+++
@@ -1,7 +0,0 @@
-package com.thoughtworks;
-
-public class WrongFormat extends RuntimeException {
-    public WrongFormat() {
-        super();
-    }
-}
---
+++ src/main/java/com/thoughtworks/WrongFormatException.java
@@ -0,0 +1,7 @@
+package com.thoughtworks;
+
+public class WrongFormatException extends RuntimeException {
+    public WrongFormatException() {
+        super();
+    }
+}
