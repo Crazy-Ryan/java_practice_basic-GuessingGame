e6759c6678ca6c8e2f320d8afc376fa276403d66
Ryan.Wang <ranwang1995@outlook.com>
2020-02-29 18:03:23 +0800

    [WangRan] feat: add a constant to store the path of the answer text file

--- src/main/java/com/thoughtworks/GuessConstant.java
+++ src/main/java/com/thoughtworks/GuessConstant.java
@@ -1,8 +1,11 @@
 package com.thoughtworks;
 
+import java.io.File;
+
 public class GuessConstant {
     public static final int COUNT = 4;
     public static final int LOWER_LIMIT = 1;
     public static final int UPPER_LIMIT = 9;
     public static final int MAX_TIMES_OF_GUESS= 6;
+    public static final String ANSWER_PATH = "src"+ File.separator + "main" + File.separator + "resources" +File.separator + "answer2.txt";
 }
