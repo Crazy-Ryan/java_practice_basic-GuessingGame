b3cba6d928cde4d8b3a1622f2fd7d6f640693232
Ryan.Wang <ranwang1995@outlook.com>
2020-02-29 17:08:26 +0800

    [WangRan] feat: add class GuessConstant with guess properties for reference

---
+++ src/main/java/com/thoughtworks/GuessConstant.java
@@ -0,0 +1,7 @@
+package com.thoughtworks;
+
+public class GuessConstant {
+    public static final int COUNT = 4;
+    public static final int LOWER_LIMIT = 1;
+    public static final int UPPER_LIMIT = 9;
+}
